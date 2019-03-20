9027b8ae8d1a9514c2273a49aa911a7b1b03572f
kwokwilliam <wkwok16@uw.edu>
2019-03-19 19:33:39 -0700

    Reinstalled packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
-            <div>
+            <Router>
+                <Switch>
 
-            </div>
+                </Switch>
+            </Router>
         );
     }
 }
