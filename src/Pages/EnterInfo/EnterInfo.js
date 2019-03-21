3ea5b69c46da99cab64d2a09c0fbe4e54d019b8c
kwokwilliam <wkwok16@uw.edu>
2019-03-20 22:27:20 -0700

    Added team selection

--- src/Pages/EnterInfo/EnterInfo.js
+++ src/Pages/EnterInfo/EnterInfo.js
@@ -1,18 +1,35 @@
 import React, { useState } from 'react';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-import Teams from '../../Assets/Teams/Teams';
+import { Teams, TeamsMapping } from '../../Assets/Teams/Teams';
 
 const EnterInfo = () => {
-    const [dropOpen, setDropOpen] = useState(false)
+    const [dropOpen, setDropOpen] = useState(false);
+    const [selectedTeam, setSelectedTeam] = useState(0);
 
-    // const
     return <>
-        <Dropdown isOpen={dropOpen} toggle={() => setDropOpen(!dropOpen)}>
+        <Dropdown isOpen={dropOpen} toggle={() => setDropOpen(!dropOpen)} >
             <DropdownToggle caret>
-                Choose a team
+                {selectedTeam !== 0 ? <>{selectedTeam} - {TeamsMapping[selectedTeam]}</> : <>Choose a team</>}
             </DropdownToggle>
-            <DropdownMenu>
-                {Teams}
+            <DropdownMenu modifiers={{
+                setMaxHeight: {
+                    enabled: true,
+                    order: 890,
+                    fn: (data) => {
+                        return {
+                            ...data,
+                            styles: {
+                                ...data.styles,
+                                overflow: 'auto',
+                                maxHeight: window.innerHeight * .8,
+                            },
+                        };
+                    },
+                },
+            }}>
+                {Teams.map(team => {
+                    return <DropdownItem onClick={() => { setSelectedTeam(team) }}>{team} - {TeamsMapping[team]}</DropdownItem>
+                })}
             </DropdownMenu>
         </Dropdown>
     </>
