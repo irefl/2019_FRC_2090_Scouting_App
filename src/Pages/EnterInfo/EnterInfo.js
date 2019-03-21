c47ee06f2bafb938712acce843f4b737da27712c
kwokwilliam <wkwok16@uw.edu>
2019-03-20 22:47:00 -0700

    Added match number picker

--- src/Pages/EnterInfo/EnterInfo.js
+++ src/Pages/EnterInfo/EnterInfo.js
@@ -1,14 +1,29 @@
 import React, { useState } from 'react';
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import {
+    Dropdown, DropdownToggle, DropdownMenu, DropdownItem,
+    Input, InputGroup, InputGroupAddon, InputGroupText
+} from 'reactstrap';
 import { Teams, TeamsMapping } from '../../Assets/Teams/Teams';
+import BlueButton from '../../Components/BlueButton';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
+const Spacer = ({ space }) => {
+    const marginTop = space || 0
+    return <div style={{ marginTop }}></div>
+}
 
 const EnterInfo = () => {
     const [dropOpen, setDropOpen] = useState(false);
     const [selectedTeam, setSelectedTeam] = useState(0);
+    const [matchNumber, setMatchNumber] = useState(0);
 
     return <>
+        <h1>Enter team information</h1>
+
+        {/** Team picker */}
         <Dropdown isOpen={dropOpen} toggle={() => setDropOpen(!dropOpen)} >
-            <DropdownToggle caret>
+            <DropdownToggle caret style={{ backgroundColor: "#20508b" }}>
                 {selectedTeam !== 0 ? <>{selectedTeam} - {TeamsMapping[selectedTeam]}</> : <>Choose a team</>}
             </DropdownToggle>
             <DropdownMenu modifiers={{
@@ -28,10 +43,30 @@ const EnterInfo = () => {
                 },
             }}>
                 {Teams.map(team => {
-                    return <DropdownItem onClick={() => { setSelectedTeam(team) }}>{team} - {TeamsMapping[team]}</DropdownItem>
+                    return <DropdownItem key={team} onClick={() => { setSelectedTeam(team) }}>{team} - {TeamsMapping[team]}</DropdownItem>
                 })}
             </DropdownMenu>
         </Dropdown>
+
+        <Spacer space={10} />
+
+        {/** Match number */}
+        <InputGroup>
+            <InputGroupAddon addonType="prepend">
+                <InputGroupText>Match Number</InputGroupText>
+            </InputGroupAddon>
+            <Input value={matchNumber !== 0 ? matchNumber : ""} placeholder="Number" type="number" onChange={(e) => {
+                let newNumber = Number(e.target.value)
+                console.log(newNumber);
+                if (newNumber > 100 || newNumber < 0) {
+                    toast.warn("Match number must be between 0 and 100")
+                } else {
+                    setMatchNumber(newNumber)
+                }
+            }} />
+        </InputGroup>
+        <hr />
+        <Link to="/"><BlueButton>Back</BlueButton></Link>
     </>
 }
 
