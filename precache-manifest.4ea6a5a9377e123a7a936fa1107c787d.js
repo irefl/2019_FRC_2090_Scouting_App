073956d8ca24f80c0025306d449525a374bd1df2
kwokwilliam <wkwok16@uw.edu>
2019-03-21 21:03:32 -0700

    Updates

---
+++ precache-manifest.4ea6a5a9377e123a7a936fa1107c787d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "711c55eeb399d4e4aea4",
+    "url": "/2019_FRC_2090_Scouting_App/static/css/main.9c7eb091.chunk.css"
+  },
+  {
+    "revision": "711c55eeb399d4e4aea4",
+    "url": "/2019_FRC_2090_Scouting_App/static/js/main.9d903494.chunk.js"
+  },
+  {
+    "revision": "3de3d3191ae6721691a7",
+    "url": "/2019_FRC_2090_Scouting_App/static/js/runtime~main.a48c6ae0.js"
+  },
+  {
+    "revision": "8280e5d7a38afccfba4e",
+    "url": "/2019_FRC_2090_Scouting_App/static/css/2.aa1a3e27.chunk.css"
+  },
+  {
+    "revision": "8280e5d7a38afccfba4e",
+    "url": "/2019_FRC_2090_Scouting_App/static/js/2.b57b2e98.chunk.js"
+  },
+  {
+    "revision": "8d295c08aa618efe7730ff42abfa1f40",
+    "url": "/2019_FRC_2090_Scouting_App/static/media/HabPlatforms.8d295c08.png"
+  },
+  {
+    "revision": "f87b9b4f34bdbf75b5c0cf3a5a137508",
+    "url": "/2019_FRC_2090_Scouting_App/static/media/Quicksand-Regular.f87b9b4f.ttf"
+  },
+  {
+    "revision": "0c64233241ead44bffbec54eb9d1d164",
+    "url": "/2019_FRC_2090_Scouting_App/static/media/Quicksand-Medium.0c642332.ttf"
+  },
+  {
+    "revision": "0c04462696ac0fd3e85e75415b483fdb",
+    "url": "/2019_FRC_2090_Scouting_App/static/media/Quicksand-Bold.0c044626.ttf"
+  },
+  {
+    "revision": "c5f954788f341b22e1974433bb972ac1",
+    "url": "/2019_FRC_2090_Scouting_App/static/media/Quicksand-Light.c5f95478.ttf"
+  },
+  {
+    "revision": "8935685ef8b3fa74b6d6d7586bdf8c08",
+    "url": "/2019_FRC_2090_Scouting_App/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/2019_FRC_2090_Scouting_App/precache-manifest.4ea6a5a9377e123a7a936fa1107c787d.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/2019_FRC_2090_Scouting_App/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
