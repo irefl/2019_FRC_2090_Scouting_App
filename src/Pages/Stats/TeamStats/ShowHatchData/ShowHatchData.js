c8b4a26577f5c68fbc25668bc1af2a148b693636
William Kwok <wkwok1998@gmail.com>
2019-03-22 19:46:19 -0700

    Refactored chart to its own component

--- src/Pages/Stats/TeamStats/ShowHatchData/ShowHatchData.js
+++ src/Pages/Stats/TeamStats/ShowHatchData/ShowHatchData.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Line } from 'recharts';
 import { hatchScores } from '../GetDataObjects/GetDataObjects';
+import MatchScoreChart from '../Charts/MatchScoreChart/MatchScoreChart';
 
 const ShowHatchData = ({ matchData }) => {
     let data = hatchScores(matchData);
     return <>
         <h3>Total Hatches Scored Per Match</h3>
-        <LineChart width={300} height={250} data={data} margin={{ top: 0, right: 0, left: 10, bottom: 25 }}>
-            <XAxis dataKey="match" domain={[1, "auto"]} allowDecimals={false} label={{ value: "Matches", position: "bottom" }} type="number" />
-            <YAxis dataKey="score" domain={["auto", "auto"]} label={{ value: "Score", angle: -90, position: "insideLeft" }} />
-            <CartesianGrid stroke="#ddd" strokeDashArray="3 3" />
-            <Tooltip />
-            <Line dot={true} type="monotone" dataKey="score" stroke={"#20508B"} fill={"#20508B"} />
-        </LineChart>
+        <MatchScoreChart data={data} />
     </>
 }
 
