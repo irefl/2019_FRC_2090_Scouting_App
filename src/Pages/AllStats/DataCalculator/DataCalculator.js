da5cf1d2554aa0dcd10d477f00ffb8671010d222
William Kwok <wkwok1998@gmail.com>
2019-03-27 20:51:53 -1000

    Updated crash on no data

--- src/Pages/AllStats/DataCalculator/DataCalculator.js
+++ src/Pages/AllStats/DataCalculator/DataCalculator.js
@@ -17,48 +17,51 @@ const DataCalculator = (data) => {
     let hatchDropCounts = {};
     let habBonuses = {};
 
-    Object.keys(data).forEach(team => {
-        const matchData = data[team] && data[team].match_data ? data[team].match_data : {}
-        let matchCount = Object.keys(matchData).length;
-        let cargoTotal = 0;
-        let hatchTotal = 0;
-        let overallTotal = 0;
-        let autonTotal = 0;
-        let rocketTotal = 0;
-        let cargoShipTotal = 0;
-        let cargoDropCount = 0;
-        let hatchDropCount = 0;
-        let habBonus = 0;
-        Object.keys(matchData).forEach(matchKey => {
-            let match = matchData[matchKey]
+    if(data) {
 
-            let countBothNearRocket = countBoth(match.nearRocket);
-            let countBothFarRocket = countBoth(match.farRocket);
-            let countBothCargoShip = countBoth(match.cargo)
-
-            cargoTotal += countCargo(match.nearRocket) + countCargo(match.farRocket) + countCargo(match.cargo);
-            hatchTotal += countHatches(match.nearRocket) + countHatches(match.farRocket) + countHatches(match.cargo);
-            overallTotal += countBothNearRocket + countBothFarRocket + countBothCargoShip;
-            autonTotal += calculateAutonLevel(match.selectedAuton)
-            rocketTotal += countBothNearRocket + countBothFarRocket;
-            cargoShipTotal += countBothCargoShip;
-            cargoDropCount += match.cargoDropped;
-            hatchDropCount += match.hatchesDropped;
-            habBonus += match.habBonus;
+        Object.keys(data).forEach(team => {
+            const matchData = data[team] && data[team].match_data ? data[team].match_data : {}
+            let matchCount = Object.keys(matchData).length;
+            let cargoTotal = 0;
+            let hatchTotal = 0;
+            let overallTotal = 0;
+            let autonTotal = 0;
+            let rocketTotal = 0;
+            let cargoShipTotal = 0;
+            let cargoDropCount = 0;
+            let hatchDropCount = 0;
+            let habBonus = 0;
+            Object.keys(matchData).forEach(matchKey => {
+                let match = matchData[matchKey]
+    
+                let countBothNearRocket = countBoth(match.nearRocket);
+                let countBothFarRocket = countBoth(match.farRocket);
+                let countBothCargoShip = countBoth(match.cargo)
+    
+                cargoTotal += countCargo(match.nearRocket) + countCargo(match.farRocket) + countCargo(match.cargo);
+                hatchTotal += countHatches(match.nearRocket) + countHatches(match.farRocket) + countHatches(match.cargo);
+                overallTotal += countBothNearRocket + countBothFarRocket + countBothCargoShip;
+                autonTotal += calculateAutonLevel(match.selectedAuton)
+                rocketTotal += countBothNearRocket + countBothFarRocket;
+                cargoShipTotal += countBothCargoShip;
+                cargoDropCount += match.cargoDropped;
+                hatchDropCount += match.hatchesDropped;
+                habBonus += match.habBonus;
+            });
+    
+            if (matchCount > 0) {
+                cargoScores[team] = cargoTotal / matchCount;
+                hatchScores[team] = hatchTotal / matchCount;
+                overallScores[team] = overallTotal / matchCount;
+                autonScores[team] = autonTotal / matchCount;
+                rocketOverallScores[team] = rocketTotal / matchCount;
+                cargoShipOverallScores[team] = cargoShipTotal / matchCount;
+                cargoDropCounts[team] = cargoDropCount / matchCount;
+                hatchDropCounts[team] = hatchDropCount / matchCount;
+                habBonuses[team] = habBonus / matchCount;
+            }
         });
-
-        if (matchCount > 0) {
-            cargoScores[team] = cargoTotal / matchCount;
-            hatchScores[team] = hatchTotal / matchCount;
-            overallScores[team] = overallTotal / matchCount;
-            autonScores[team] = autonTotal / matchCount;
-            rocketOverallScores[team] = rocketTotal / matchCount;
-            cargoShipOverallScores[team] = cargoShipTotal / matchCount;
-            cargoDropCounts[team] = cargoDropCount / matchCount;
-            hatchDropCounts[team] = hatchDropCount / matchCount;
-            habBonuses[team] = habBonus / matchCount;
-        }
-    });
+    }
 
     return {
         cargoScores,
